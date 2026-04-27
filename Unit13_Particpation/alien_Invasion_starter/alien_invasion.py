769341dcd96ce4fe0a180aea6423e3ff63e17520
zostheimer-cscc <zostheimer@student.cscc.edu>
2026-04-26 21:45:22 -0400

    Commit 2
    Updated settings.py, updated invasion.py (typo in commit 1, instead of updated settings.py updated invasion.py)

--- Unit13_Particpation/alien_Invasion_starter/alien_invasion.py
+++ Unit13_Particpation/alien_Invasion_starter/alien_invasion.py
@@ -7,9 +7,15 @@ from alien_fleet import AlienFleet
 
 
 class AlienInvasion:
+
     def __init__(self) -> None:
+        pygame.init()
+        self.settings = Settings()
+        self.settings.initialize_dynamic_settings()
+        self.game_stats = GameStats(self.settings.starting_ship_count)
+
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_w,self.settings.screen_h)
+            (self.settings.screen_w, self.settings.screen_h)
         )
         pygame.display.set_caption(self.settings.name)
 
@@ -21,78 +27,93 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
-        pygame.mixer_init()
+        pygame.mixer.init()
         self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
         self.laser_sound.set_volume(0.7)
 
+        self.impact_sound = pygame.mixer.Sound(self.settings.impact_sound)
+        self.impact_sound.set_volume(0.7)
+
+
         self.ship = Ship(self, Arsenal(self))
         self.alien_fleet = AlienFleet(self)
-        self.alien_fleet = create_fleet()
+        self.alien_fleet.create_fleet()
+
+        self.play_button = Button(self, 'Play')
+        self.game_active = False
 
     def run_game(self) -> None:
-        # Game loop
+        # Game Loop
         while self.running:
             self._check_events()
-            self.ship.update()
-            #self.alien.update()
+            if self.game_active:
+                self.ship.update()
+                self.alien_fleet.update_fleet()
+                self._check_collisions()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
+
+    def _check_collisions(self) -> None:
+        # check collisions for ship
+        if self.ship.check_collisions(self.alien_fleet.fleet):
+            self._check_game_status()
+            # subtract one life if possible
+
+        # check collisions for aliens and bottom of screen
+        if self.alien_fleet.check_fleet_bottom():
+            self._check_game_status()
+        # check collisions of projectiles and aliens
+        collisions = self.alien_fleet.check_collisions(self.ship.arsenal.arsenal)
+        if collisions:
+            self.impact_sound.play()
+            self.impact_sound.fadeout(500)
+
+        if self.alien_fleet.check_destroyed_status():
+            self._reset_level()
+            self.settings.increase_difficulty()
+            # update game stats level
+            # update HUD view
+
+    def _check_game_status(self) -> None:
+
+        if self.game_stats.ships_left > 0:
+            self.game_stats.ships_left -= 1
+            self._reset_level()
+            sleep(0.5)
+        else:
+            self.game_active = False
+
+
+    def _reset_level(self) -> None:
+        self.ship.arsenal.arsenal.empty()
+        self.alien_fleet.fleet.empty()
+        self.alien_fleet.create_fleet()
+
+
+    def restart_game(self) -> None:
+        self.settings.initialize_dynamic_settings()
+        # reset Game stats
+        # update HUD scores
+        self._reset_level()
+        self.ship._center_ship()
+        self.game_active = True
+        pygame.mouse.set_visible(False)
 
     def _update_screen(self) -> None:
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
-        pygame.display.flip()
+        # draw HUD
 
         if not self.game_active:
-        self.play_button.draw()
-        pygame.mouse.set_visible(True)
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
 
-    pygame.display.flip()
+        pygame.display.flip()
 
-    def _reset_level(self) -> None:
-        self.ship.arsenal.arsenal.empty()
-        self.alien_fleet.fleet.empty()
-        elf.alien_fleet.create_fleet()
-
-
-   def _check_events(self) -> None:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            self.running = False
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN and self.game_active == True:
-            self._check_keydown_events(event)
-        elif event.type == pygame.KEYUP:
-            self._check_keyup_events(event)
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            self._check_button_clicked()
-
-    def _check_button_clicked(self) -> None:
-    mouse_pos = pygame.mouse.get_pos()
-    if self.play_button.check_clicked(mouse_pos):
-        self.restart_game()
-
-    def _check_keyup_events(self, event) -> None:
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
-
-    def _check_keydown_events(self, event) -> None:
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
-        elif event.key == pygame.K_SPACE:
-            if self.ship.fire():
-                self.laser_sound.play()
-                self.laser_sound.fadeout(250)
-
-        elif event.key == pygame.K_q:
-            self.running = False
-            pygame.quit()
+    def _check_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
             sys.exit()
 
 
--- Unit13_Particpation/alien_Invasion_starter/settings.py
+++ Unit13_Particpation/alien_Invasion_starter/settings.py
@@ -2,27 +2,28 @@ from pathlib import Path
 class Settings:
 
     def __init__(self) -> None:
-        self.name: str = 'Alien Invasion'
-        self.screen_w = 1200
-        self.screen_h = 800
-        self.FPS = 60
-        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+        self.button_w = 200
+        self.button_h = 50
+        self.button_color = (0,135,50)
 
-        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
-        self.ship_w = 40
-        self.ship_h = 60
+        self.text_color = (255,255,255)
+        self.button_font_size = 48
+        self.HUD_font_size = 20
+        self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Silkscreen' / 'Silkscreen-Bold.ttf'
+
+    def initialize_dynamic_settings(self) -> None:
         self.ship_speed = 5
+        self.starting_ship_count = 3
 
-        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
-        self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
-        self.bullet_speed = 7
-        self.bullet_w = 25
+        self.bullet_w = 250
         self.bullet_h = 80
+        self.bullet_speed = 7
         self.bullet_amount = 5
 
-        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
-        self.alien_w = 80
-        self.alien_h = 80
-        self.fleet_speed = 5
-        self.fleet_direction = 1
+        self.fleet_speed = 2
         self.fleet_drop_speed = 40
+
+    def increase_difficulty(self) -> None:
+        self.ship_speed *= self.difficulty_scale
+        self.bullet_speed *= self.difficulty_scale
+        self.fleet_speed *= self.difficulty_scale
